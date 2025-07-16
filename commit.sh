9a2f69cf65be2bb384f9c713c3cd9a290ce54fb0
afriola <finalafriola@gmail.com>
2025-07-16 08:39:01 +0700

    20250716 08:39

--- commit.sh
+++ commit.sh
@@ -36,7 +36,7 @@ while read -r category; do
 done <<< "$categories"
 
 echo ""
-echo -e "\e[1;37;42mREADME.md berhasil dibuat.\e[0m"
+echo -e "\x1b[1;37;42mREADME.md berhasil dibuat.\x1b[0m"
 
 
 # PUSH KE GITHUB
