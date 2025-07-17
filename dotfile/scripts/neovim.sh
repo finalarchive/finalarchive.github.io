08f559aad44d9afbbd993c723be38d12b0a2cdc7
afriola <finalafriola@gmail.com>
2025-07-17 10:32:42 +0700

    20250717 10:32

--- dotfile/scripts/neovim.sh
+++ dotfile/scripts/neovim.sh
@@ -3,15 +3,5 @@
 # CATEGORY: Editor
 # WAJIB: no
 
-echo "🚀 Menghapus Neovim lama..."
-sudo apt remove neovim -y
-
-echo "⬇️  Mengunduh Neovim versi terbaru dari GitHub..."
-cd /tmp
-curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
-sudo rm -rf /opt/nvim-linux-x86_64
-sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
-export PATH="$PATH:/opt/nvim-linux-x86_64/bin"
-echo 'export PATH="$PATH:/opt/nvim-linux-x86_64/bin"' >> ~/.bashrc
-source ~/.bashrc
+sudo apt install neovim -y
 nvim --version
